4ccd9f47349c670cee78dc528b4d707a8a5c8302
TaeKwonZeus <dyatlovleonid@gmail.com>
2020-11-29 20:50:01 +0300

    added shit

--- Assets/Scripts/StartMenu/LobbyNetworkManager.cs
+++ Assets/Scripts/StartMenu/LobbyNetworkManager.cs
@@ -1,9 +1,20 @@
 ﻿using Mirror;
+using UnityEngine.SceneManagement;
 
 namespace StartMenu
 {
     public class LobbyNetworkManager : NetworkManager
     {
-	    
+        public void OnHost()
+        {
+            SceneManager.LoadScene("Main");
+            StartHost();
+        }
+
+        public void OnClient()
+        {
+            SceneManager.LoadScene("Main");
+            StartClient();
+        }
     }
 }
